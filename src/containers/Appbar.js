09554d95926d8566b2040dcdf0c996f98704246d
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2022-06-19 14:47:55 +0530

    adds export-to-excel functionality

--- src/containers/Appbar.js
+++ src/containers/Appbar.js
@@ -6,11 +6,42 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
 import InsertContent from '../components/InsertContent';
+import { connect } from 'react-redux'
+import * as XLSX from 'xlsx';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
+const Appbar = ({contents}) => {
 
-const Appbar = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const [openNewContentDialog, setOpenNewContentDialog] = React.useState(false);
+
+  const downloadExcel = (type) => {
+    let data
+    if(type === 'To Do' ){
+      data = contents.db[0]['data']
+    }
+    if(type === 'Monthly Accomplishments'){
+      data = contents.db[1]['data']
+    }
+    if(type === 'Awards'){
+      data = contents.db[2]['data']
+    }
+
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+    XLSX.writeFile(workbook, "DataSheet.xlsx");
+    handleClose()
+  };
 
   const addNewContent = () => {
     setOpenNewContentDialog(true)
@@ -24,9 +55,32 @@ const Appbar = () => {
                     Planner
                 </Typography>
                 <div style={{display:'flex'}}>
-                  <DownloadForOfflineIcon fontSize='small' sx={{marginLeft:'8px'}}/>
+                  <div>
+                    <DownloadForOfflineIcon
+                      id="basic-button"
+                      aria-controls={open ? 'basic-menu' : undefined}
+                      aria-haspopup="true"
+                      aria-expanded={open ? 'true' : undefined}
+                      onClick={handleClick}
+                      fontSize='small'
+                      sx={{marginLeft:'8px'}}
+                    />
+                    <Menu
+                      id="basic-menu"
+                      anchorEl={anchorEl}
+                      open={open}
+                      onClose={handleClose}
+                      MenuListProps={{
+                        'aria-labelledby': 'basic-button',
+                      }}
+                    >
+                      <MenuItem onClick={() => downloadExcel('To Do')}>Export - To Do</MenuItem>
+                      <MenuItem onClick={() => downloadExcel('Monthly Accomplishments')}>Export - Monthly Accomplishments</MenuItem>
+                      <MenuItem onClick={() => downloadExcel('Awards')}>Export - Awards</MenuItem>
+                    </Menu>
+                  </div>
                   <AddCircleIcon fontSize='small' sx={{marginLeft:'8px'}} onClick={addNewContent}/>
-                  <AccountCircleIcon fontSize='small' sx={{marginLeft:'8px'}}/>
+                  <AccountCircleIcon fontSize='small' sx={{marginLeft:'8px'}} />
                 </div>
             </div>
         </Toolbar>
@@ -35,4 +89,8 @@ const Appbar = () => {
   );
 }
 
-export default Appbar
+const mapStateToProps = (state) => ({
+    contents  : state.contents
+})
+
+export default connect(mapStateToProps,null)(Appbar)
